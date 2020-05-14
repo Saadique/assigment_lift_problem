89679f3cc0fe9617cb49884d229b4ace20e65617
Saadique <48992310+Saadique@users.noreply.github.com>
2020-05-14 07:05:10 +0530

    Update LiftApplication.java

--- lift/src/main/java/com/SmartKent/lift/LiftApplication.java
+++ lift/src/main/java/com/SmartKent/lift/LiftApplication.java
@@ -9,20 +9,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 
 @SpringBootApplication
-@EnableAsync
 public class LiftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiftApplication.class, args);
-	}
-
-	@Bean
-	public Executor executor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(5);
-		executor.setMaxPoolSize(5);
-		executor.setQueueCapacity(500);
-		executor.initialize();
-		return executor;
 	}
 }
